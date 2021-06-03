a0d4e08db1e1afcab53707a6335c29db4d1fce94
Nikola <nsretkovic@elfak.rs>
2021-06-03 17:59:07 +0200

    post component

--- domaci5/src/app/app.module.ts
+++ domaci5/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ domaci5/src/app/models/post.ts
@@ -0,0 +1,10 @@
+export interface Post{
+    name: string,
+    username:string,
+    userPicture:string,
+    description:string,
+    postPicture:string,
+    likes:number,
+    dislikes:number,
+    shares:number
+}
